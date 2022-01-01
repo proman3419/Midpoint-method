c723d9ea5e66e9356657a77cf18723a50f0edec7
proman3419 <proman7268@gmail.com>
2022-01-01 15:23:00 +0100

    The graphs are displayed in one window frame by frame

--- midpoint.py
+++ midpoint.py
@@ -2,68 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import exp
 import argparse
-
-
-# LOGIC ########################################################################
-def f_deriv(A, B, t, x):
-    return A*x + B*exp(-t)
-
-
-def calculate_C(A, B, a, xa):
-    return (xa * (A+1) + B*exp(-a)) / (exp(A*a) * (A+1))
-
-
-def f(A, B, C, t, xa):
-    return C*exp(A*t) - ((B*exp(-t)) / (A+1))
-
-
-def calculate_xs_act(A, B, C, ts, xa):
-    xs_act = []
-
-    for t in ts:
-        xs_act.append(f(A, B, C, t, xa))
-
-    return xs_act
-
-
-def midpoint(A, B, xa, ts):
-    N = len(ts)
-    xs = [0]*N
-    xs[0] = xa
-
-    for i in range(N-1):
-        h = ts[i+1] - ts[i]
-        xk1 = xs[i] + h/2 * f_deriv(A, B, ts[i], xs[i])
-        xk2 = xs[i] + h * f_deriv(A, B, ts[i] + h/2, xk1)
-        xs[i+1] = xk2
-
-    return xs
-
-
-def calculate_precision(xs, xs_act):
-    n = len(xs)
-    return sum(abs(xs[i] - xs_act[i]) for i in range(n)) / n
-
-
-def midpoint_with_precision(A, B, a, b, n, xa, E):
-    C = calculate_C(A, B, a, xa)
-
-    while True:
-        ts = np.linspace(a, b, n)
-        xs_act = calculate_xs_act(A, B, C, ts, xa)
-        xs = midpoint(A, B, xa, ts)
-        prec = calculate_precision(xs, xs_act)
-
-        plt.plot(ts, xs, label='calculated')
-        plt.plot(ts, xs_act, label='actual')
-        plt.legend()
-        plt.show()
-        
-        if prec <= E:
-            return (xs, xs_act)
-
-        n *= 2
-# LOGIC ########################################################################
+import time
 
 
 # INPUT ########################################################################
@@ -108,8 +47,102 @@ def get_args():
 
 
 # OUTPUT #######################################################################
+def init_graph_displayer(ts, xs_act):
+    plt.ion()
+    fig = plt.figure()
 
+    update_graph(fig, [ts], [xs_act], ['actual'])
+
+    return fig
+
+
+def update_graph(fig, ts_list, xs_list, labels):
+    fig.clf()
+    graph = fig.add_subplot(111)
+
+    for i in range(len(labels)):
+        graph.plot(ts_list[i], xs_list[i], label=labels[i])
+
+    plt.legend()
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    fig.clf()
+
+
+def output(fig, n, ts, xs_act, xs, prec):
+    print(n, xs_act, xs, prec)
+    update_graph(fig, [ts, ts], [xs_act, xs], ['actual', 'calculated'])
 # OUTPUT #######################################################################
+
+
+# LOGIC ########################################################################
+def f_deriv(A, B, t, x):
+    return A*x + B*exp(-t)
+
+
+def calculate_C(A, B, a, xa):
+    return (xa * (A+1) + B*exp(-a)) / (exp(A*a) * (A+1))
+
+
+def f(A, B, C, t, xa):
+    return C*exp(A*t) - ((B*exp(-t)) / (A+1))
+
+
+def calculate_xs_act(A, B, C, ts, xa):
+    xs_act = []
+
+    for t in ts:
+        xs_act.append(f(A, B, C, t, xa))
+
+    return xs_act
+
+
+def midpoint(A, B, xa, ts):
+    N = len(ts)
+    xs = [0]*N
+    xs[0] = xa
+
+    for i in range(N-1):
+        h = ts[i+1] - ts[i]
+        xk1 = xs[i] + h/2 * f_deriv(A, B, ts[i], xs[i])
+        xk2 = xs[i] + h * f_deriv(A, B, ts[i] + h/2, xk1)
+        xs[i+1] = xk2
+
+    return xs
+
+
+def calculate_precision(xs, xs_act):
+    n = len(xs)
+    return sum(abs(xs[i] - xs_act[i]) for i in range(n)) / n
+
+
+def midpoint_iteration(A, B, C, a, b, n, xa):
+    ts = np.linspace(a, b, n)
+    xs_act = calculate_xs_act(A, B, C, ts, xa)
+    xs = midpoint(A, B, xa, ts)
+    prec = calculate_precision(xs, xs_act)
+
+    return (ts, xs_act, xs, prec)
+
+
+def midpoint_with_precision(A, B, a, b, n, xa, E):
+    C = calculate_C(A, B, a, xa)
+    ts = np.linspace(a, b, n)
+    xs_act = calculate_xs_act(A, B, C, ts, xa)
+    fig = init_graph_displayer(ts, xs_act)
+
+    while True:
+        ts, xs_act, xs, prec = midpoint_iteration(A, B, C, a, b, n, xa)
+        
+        if prec <= E:
+            return (xs, xs_act)
+
+        output(fig, n, ts, xs_act, xs, prec)
+
+        n *= 2
+
+        time.sleep(1)
+# LOGIC ########################################################################
 
 
 if __name__ == '__main__':
